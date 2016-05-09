2f5b4b1221a0af5764da9c180f14dce8ccc615f0
patb23 <shruthi@PM.local>
2016-05-09 03:38:46 -0400

    initial

---
+++ factual-dc/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name factualDcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the factualDcApp
+ */
+angular.module('factualDcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ factual-dc/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name factualDcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the factualDcApp
+ */
+angular.module('factualDcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
